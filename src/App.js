5e07eae57fb39469fc3463922bed1f0f96c523c7
JohnCodePass <jmoralesc0259@gmail.com>
2021-01-16 02:45:41 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 function App() {
   return (
-    <h1>Hello there</h1>
+    <Container fluid className="contArea">
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <h1 className="titleArea">In the center</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
