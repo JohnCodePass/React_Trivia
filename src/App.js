699649a1daee83354b738d2f29ced5b896329f9b
JohnCodePass <jmoralesc0259@gmail.com>
2021-01-18 00:26:21 -0800

    Small Changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <Container fluid className="contArea">
+    <div className="App">
+
+    <Container fluid className="contArea d-flex justify-content-center align-items-center">
       <Row>
         <Col className="d-flex justify-content-center">
-          <h1 className="titleArea">In the center</h1>
+          <h1 className="titleArea">Scuffed Trivia</h1>
         </Col>
       </Row>
     </Container>
+
+    </div>
   );
 }
 
