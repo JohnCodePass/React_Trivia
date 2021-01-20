ca8f01dc5d2dda3f9b4de838010ed995278f3160
JohnCodePass <jmoralesc0259@gmail.com>
2021-01-19 18:50:59 -0800

    Minor changes

--- src/components/maintitle/title.js
+++ src/components/maintitle/title.js
@@ -10,13 +10,12 @@ import {
 
 import './title.css'
 
-
 const maintitle = () => {
     return (
 
         <Router>
 
-            <Container fluid className="contArea d-flex justify-content-center align-items-center flex-column">
+            <Container fluid id='titleId' className="contArea d-flex justify-content-center align-items-center flex-column">
                 <Row>
                     <Col className="d-flex justify-content-center">
                         <h1 className="titleArea">Scuffed Trivia</h1>
@@ -25,8 +24,10 @@ const maintitle = () => {
                 <Row>
                     <Col className="d-flex justify-content-center flex-column">
                         <Button variant="success" className="mt-5"><h1>Easy</h1></Button>
-                        <Button variant="warning" className="my-5"><h1>Medium</h1></Button>
-                        <Button variant="danger" className="mb-5"><h1>Hard</h1></Button>
+                        <Button variant="warning" className="my-5"><h1>Hard</h1></Button>
+                    </Col>
+                    <Col className="d-flex justify-content-center flex-column">
+                        <Button variant="danger" className="mb-5"><h1>Medium</h1></Button>
                         <Button variant="dark" as={Link} to='/options'><h1>Options</h1></Button>
                     </Col>
                 </Row>
--- src/components/options/optionpage.js
+++ src/components/options/optionpage.js
@@ -1,9 +1,21 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import './options.css';
 
 
 const optionspage = () => {
-    return(
-        <p>This is going to be the options page</p>
+    return (
+        <Container className="contArea">
+            <Row>
+                <Col>
+                    <p>This is going to be the options page</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center flex-column">
+                    <Button variant="success" className="mt-5"><h1>Main Menu</h1></Button>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
