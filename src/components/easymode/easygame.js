149f98bc86c1dc3df990f6248660544267cf92e2
JohnCodePass <jmoralesc0259@gmail.com>
2021-01-21 01:12:56 -0800

    Small edits

--- src/components/easymode/easygame.js
+++ src/components/easymode/easygame.js
@@ -2,40 +2,40 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import './easygame.css'
 import { Link } from 'react-router-dom'
 
-let counter = 0;
+let counter = 1;
 
 let easyQs = [
     {
-        "Question": "Who is swore in as president in 2021?",
-        'Ans1': 'JT',
-        'Ans2': 'Joseph',
-        'Ans3': 'Trump',
-        'Ans4': 'Biden',
-        'corAns': 'Biden'
+        Question: "Who is swore in as president in 2021?",
+        Ans1: 'JT',
+        Ans2: 'Joseph',
+        Ans3: 'Trump',
+        Ans4: 'Biden',
+        corAns: 'Biden'
     },
     {
-        "Question": "What is the highest rank in league of legends?",
-        'Ans1': 'Master',
-        'Ans2': 'GrandMaster',
-        'Ans3': 'Challenger',
-        'Ans4': 'Iron',
-        'corAns': 'Challenger'
+        Question: "What is the highest rank in league of legends?",
+        Ans1: 'Master',
+        Ans2: 'GrandMaster',
+        Ans3: 'Challenger',
+        Ans4: 'Iron',
+        corAns: 'Challenger'
     },
     {
-        "Question": "As of December 2020, what is the latest series of iphone?",
-        'Ans1': 'iphone 10',
-        'Ans2': 'iphone 12',
-        'Ans3': 'iphone 11',
-        'Ans4': 'iphone 13',
-        'corAns': 'iphone 12'
+        Question: "As of December 2020, what is the latest series of iphone?",
+        Ans1: 'iphone 10',
+        Ans2: 'iphone 12',
+        Ans3: 'iphone 11',
+        Ans4: 'iphone 13',
+        corAns: 'iphone 12'
     },
     {
-        "Question": "Who is the ceo of Tesla?",
-        'Ans1': 'Bill Gates',
-        'Ans2': 'Warren Buffet',
-        'Ans3': 'Elon Musk',
-        'Ans4': 'Jeff Bezos',
-        'corAns': 'Elon Musk'
+        Question: "Who is the ceo of Tesla?",
+        Ans1: 'Bill Gates',
+        Ans2: 'Warren Buffet',
+        Ans3: 'Elon Musk',
+        Ans4: 'Jeff Bezos',
+        corAns: 'Elon Musk'
     }
 ].sort(() => Math.random() - 0.5);
 
