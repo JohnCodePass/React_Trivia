82cfdd134d99f8524e603954f8a729225f2dc015
JohnCodePass <jmoralesc0259@gmail.com>
2021-01-20 19:43:33 -0800

    Easy mode WIP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Maintitle from './components/maintitle/title'
 import Optionpage from './components/options/optionpage.js'
+import Easygame from './components/easymode/easygame'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
@@ -9,9 +10,9 @@ function App() {
   return (
     <Router>
       <div className="App">
-
         <Switch>
           <Route path='/' exact component={Maintitle} />
+          <Route path='/easygame' component={Easygame} />
           <Route path='/options' component={Optionpage} />
         </Switch>
       </div>
---
+++ src/components/easymode/easygame.js
@@ -0,0 +1,41 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+
+let easyQs = [
+    {
+        "Question": "Who is swore in as president in 2021?",
+        'Ans1': 'JT',
+        'Ans2': 'Joseph',
+        'Ans3': 'Trump',
+        'Ans4': 'Biden',
+        'corAns': 'Biden'
+    },
+    {
+        "Question": "What is the highest rank in league of legends?",
+        'Ans1': 'Master',
+        'Ans2': 'GrandMaster',
+        'Ans3': 'Challenger',
+        'Ans4': 'Iron',
+        'corAns': 'Challenger'
+    },
+    {
+        "Question": "As of December 2020, what is the latest series of iphone?",
+        'Ans1': 'iphone 10',
+        'Ans2': 'iphone 12',
+        'Ans3': 'iphone 11',
+        'Ans4': 'iphone 13',
+        'corAns': 'iphone 12'
+    }
+];
+
+const easygame = () => {
+    return(
+        <Container>
+
+
+        </Container>
+
+    );
+}
+
+export default easygame;
